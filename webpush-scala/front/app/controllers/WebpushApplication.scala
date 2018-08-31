b20ee62ba2e9494c058698fcf45287da76292868
Naofumi Okamoto <naofumi_okamoto@dwango.co.jp>
2018-08-31 19:19:04 +0900

    Implement test push subscription

--- webpush-scala/front/app/controllers/WebpushApplication.scala
+++ webpush-scala/front/app/controllers/WebpushApplication.scala
@@ -1,13 +1,26 @@
 package controllers
 
 import javax.inject.Inject
+import javax.inject.Singleton
+import models.PushSubscription
+import nokamoto.protobuf.PushSubscriptionNotification
+import nokamoto.protobuf.WebpushServiceGrpc.WebpushServiceStub
+import play.api.libs.json.Json
 import play.api.mvc.AbstractController
 import play.api.mvc.Action
 import play.api.mvc.AnyContent
 import play.api.mvc.ControllerComponents
 
-class WebpushApplication @Inject()(cc: ControllerComponents)
+import scala.concurrent.ExecutionContext
+import scala.util.control.NonFatal
+
+@Singleton
+class WebpushApplication @Inject()(webpushServiceStub: WebpushServiceStub,
+                                   cc: ControllerComponents)
     extends AbstractController(cc) {
+
+  private[this] implicit val ec: ExecutionContext = cc.executionContext
+
   def index: Action[AnyContent] =
     Action(Ok(views.html.WebpushApplication.index()))
 
@@ -15,5 +28,17 @@ class WebpushApplication @Inject()(cc: ControllerComponents)
 
   def unsubscribe: Action[AnyContent] = TODO
 
-  def testSubscription: Action[AnyContent] = TODO
+  def testSubscription: Action[PushSubscription] =
+    Action.async(parse.json[PushSubscription]) { req =>
+      val m = PushSubscriptionNotification().update(
+        _.subscription := req.body.proto :: Nil,
+        _.request.content := "test subscription")
+      webpushServiceStub
+        .sendPushSubscriptionNotification(m)
+        .map(_ => Ok(Json.obj()))
+        .recover {
+          case NonFatal(e) =>
+            InternalServerError(Json.obj("error" -> e.getMessage))
+        }
+    }
 }
--- webpush-scala/front/test/controllers/WebpushApplicationSpec.scala
+++ webpush-scala/front/test/controllers/WebpushApplicationSpec.scala
@@ -1,19 +1,27 @@
 package controllers
 
+import com.google.protobuf.empty.Empty
+import models.PushSubscription
+import nokamoto.protobuf.PushSubscriptionNotification
+import nokamoto.protobuf.WebpushServiceGrpc.WebpushServiceStub
+import org.mockito.ArgumentCaptor
+import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
-import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.mvc.Results
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import org.mockito.Mockito._
+import org.mockito.Matchers._
 
-class WebpushApplicationSpec
-    extends PlaySpec
-    with GuiceOneAppPerTest
-    with Results {
+import scala.concurrent.Future
+
+class WebpushApplicationSpec extends PlaySpec with MockitoSugar with Results {
+
   "WebpushApplication#index" should {
     "return OK" in {
-      val sut = app.injector.instanceOf[WebpushApplication]
-      val actual = sut.index(FakeRequest())
+      val sut = new WebpushApplication(mock[WebpushServiceStub],
+                                       stubControllerComponents())
+      val actual = sut.index()(FakeRequest())
 
       assert(status(actual) === OK)
     }
@@ -21,8 +29,9 @@ class WebpushApplicationSpec
 
   "WebpushApplication#subscribe" should {
     "be not implemented yet" in {
-      val sut = app.injector.instanceOf[WebpushApplication]
-      val actual = sut.subscribe(FakeRequest())
+      val sut = new WebpushApplication(mock[WebpushServiceStub],
+                                       stubControllerComponents())
+      val actual = sut.subscribe()(FakeRequest())
 
       assert(status(actual) === NOT_IMPLEMENTED)
     }
@@ -30,19 +39,40 @@ class WebpushApplicationSpec
 
   "WebpushApplication#unsubscribe" should {
     "be not implemented yet" in {
-      val sut = app.injector.instanceOf[WebpushApplication]
-      val actual = sut.unsubscribe(FakeRequest())
+      val sut = new WebpushApplication(mock[WebpushServiceStub],
+                                       stubControllerComponents())
+      val actual = sut.unsubscribe()(FakeRequest())
 
       assert(status(actual) === NOT_IMPLEMENTED)
     }
   }
 
   "WebpushApplication#testSubscription" should {
-    "be not implemented yet" in {
-      val sut = app.injector.instanceOf[WebpushApplication]
-      val actual = sut.testSubscription(FakeRequest())
+    "send a push subscription notification and return OK" in {
+      val endpoint =
+        "https://updates.push.services.mozilla.com:443/wpush/v2/gAAAAABbiL"
+      val p256dh =
+        "BFn48GVRHcj1rWMiuQ0E9LqHB5MWq0OEq8T5hhiO8Mjr3O+mv3BrNLtx+bhwaV8WRJ6kQg1Q0OrtTzIyduDSPQI="
+      val auth = "xPae4nGweJlQkdLMUppMKQ=="
+      val req =
+        PushSubscription(endpoint = endpoint, p256dh = p256dh, auth = auth)
 
-      assert(status(actual) === NOT_IMPLEMENTED)
+      val stub = mock[WebpushServiceStub]
+      when(stub.sendPushSubscriptionNotification(any()))
+        .thenReturn(Future.successful(Empty()))
+
+      val sut = new WebpushApplication(stub, stubControllerComponents())
+      val actual = sut.testSubscription()(FakeRequest().withBody(req))
+
+      assert(status(actual) === OK)
+
+      val captor =
+        ArgumentCaptor.forClass(classOf[PushSubscriptionNotification])
+      verify(stub, times(1)).sendPushSubscriptionNotification(captor.capture())
+
+      val subscriptions = captor.getValue.subscription
+      assert(subscriptions.length === 1)
+      assert(req.proto === subscriptions.head)
     }
   }
 }

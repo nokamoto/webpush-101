eebca76952b059b26c1bb0244efd76189a9cb491
Naofumi Okamoto <naofumi_okamoto@dwango.co.jp>
2018-09-14 16:15:06 +0900

    Call push subscription service from front service

--- webpush-scala/front/app/controllers/WebpushApplication.scala
+++ webpush-scala/front/app/controllers/WebpushApplication.scala
@@ -4,21 +4,25 @@ import io.grpc.StatusRuntimeException
 import javax.inject.Inject
 import javax.inject.Singleton
 import models.PushSubscription
-import nokamoto.protobuf.PushSubscriptionNotification
+import nokamoto.protobuf.PushSubscriptionServiceGrpc.PushSubscriptionServiceStub
 import nokamoto.protobuf.WebpushServiceGrpc.WebpushServiceStub
-import play.api.libs.json.Json
-import play.api.mvc.AbstractController
-import play.api.mvc.Action
-import play.api.mvc.AnyContent
-import play.api.mvc.ControllerComponents
+import nokamoto.protobuf.PushSubscriptionNotification
+import nokamoto.protobuf.User
+import nokamoto.protobuf.UserSubscription
 import play.api.Logger
+import play.api.libs.json.Json
+import play.api.libs.json.Reads
+import play.api.mvc._
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 import scala.util.control.NonFatal
 
 @Singleton
-class WebpushApplication @Inject()(webpushServiceStub: WebpushServiceStub,
-                                   cc: ControllerComponents)
+class WebpushApplication @Inject()(
+    webpushServiceStub: WebpushServiceStub,
+    pushSubscriptionServiceStub: PushSubscriptionServiceStub,
+    cc: ControllerComponents)
     extends AbstractController(cc) {
 
   private[this] implicit val ec: ExecutionContext = cc.executionContext
@@ -26,26 +30,36 @@ class WebpushApplication @Inject()(webpushServiceStub: WebpushServiceStub,
   def index: Action[AnyContent] =
     Action(Ok(views.html.WebpushApplication.index()))
 
-  def subscribe: Action[AnyContent] = TODO
+  private[this] def action[A](f: Request[A] => Future[_])(
+      implicit r: Reads[A]): Action[A] = Action.async(parse.json[A]) { req =>
+    f(req).map(_ => Ok(Json.obj())).recover {
+      case e: StatusRuntimeException =>
+        Logger.error(e.getStatus.toString)
+        InternalServerError(Json.obj("error" -> e.getMessage))
 
-  def unsubscribe: Action[AnyContent] = TODO
+      case NonFatal(e) =>
+        Logger.error(e.getMessage)
+        InternalServerError(Json.obj("error" -> e.getMessage))
+    }
+  }
 
-  def testSubscription: Action[PushSubscription] =
-    Action.async(parse.json[PushSubscription]) { req =>
+  def subscribe: Action[PushSubscription] = action[PushSubscription] { req =>
+    val user = User() // TODO
+    val m = UserSubscription().update(_.user := user,
+                                      _.subscription := req.body.proto :: Nil)
+    pushSubscriptionServiceStub.subscribe(m)
+  }
+
+  def unsubscribe: Action[PushSubscription] = action[PushSubscription] { req =>
+    val m = req.body.proto
+    pushSubscriptionServiceStub.unsubscribe(m)
+  }
+
+  def testSubscription: Action[PushSubscription] = action[PushSubscription] {
+    req =>
       val m = PushSubscriptionNotification().update(
         _.subscription := req.body.proto :: Nil,
         _.request.content := "test subscription")
-      webpushServiceStub
-        .sendPushSubscriptionNotification(m)
-        .map(_ => Ok(Json.obj()))
-        .recover {
-          case e: StatusRuntimeException =>
-            Logger.error(e.getStatus.toString)
-            InternalServerError(Json.obj("error" -> e.getMessage))
-
-          case NonFatal(e) =>
-            Logger.error(e.getMessage)
-            InternalServerError(Json.obj("error" -> e.getMessage))
-        }
-    }
+      webpushServiceStub.sendPushSubscriptionNotification(m)
+  }
 }
